5c7b4aee926e94c398f2fcabd42c587411a99b4f
TMON\playjun <playjun@tmon.co.kr>
2019-11-19 18:20:53 +0900

    add init

--- src/main/structure/Map.java
+++ src/main/structure/Map.java
@@ -3,8 +3,11 @@ package structure;
 import java.io.Serializable;
 import java.util.Collection;
 import java.util.Comparator;
+import java.util.ConcurrentModificationException;
 import java.util.Objects;
 import java.util.Set;
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
 
 public interface Map<K, V> {
 
@@ -60,7 +63,79 @@ public interface Map<K, V> {
                     (c1, c2) -> cmp.compare(c1.getKey(), c2.getKey());
         }
 
+        public static <K, V> Comparator<Map.Entry<K, V>> comparingByValue(Comparator<? super V> cmp) {
+            Objects.requireNonNull(cmp);
+            return (Comparator<Map.Entry<K, V>> & Serializable)
+                    (c1, c2) -> cmp.compare(c1.getValue(), c2.getValue());
+        }
+
     }
 
+    boolean equals(Object o);
+
+    int hashCode();
+
+    default V getOrDefault(Object key, V defaultValue) {
+        V v;
+        return (((v = get(key)) != null) || containsKey(key)) ?
+                v : defaultValue;
+    }
+
+    default void forEach(BiConsumer<? super K, ? super V> action) {
+        Objects.requireNonNull(action);
+        for (Map.Entry<K, V> entry : entrySet()) {
+            K k;
+            V v;
+            try {
+                k = entry.getKey();
+                v = entry.getValue();
+            } catch (IllegalStateException ise) {
+                throw new ConcurrentModificationException();
+            }
+            action.accept(k, v);
+        }
+    }
+
+    default void replaceAll(BiFunction<? super K, ? super V, ? extends V> function) {
+        Objects.requireNonNull(function);
+        for (Map.Entry<K, V> entry : entrySet()) {
+            K k;
+            V v;
+            try {
+                k = entry.getKey();
+                v = entry.getValue();
+            } catch (IllegalStateException ise) {
+                throw new ConcurrentModificationException(ise);
+            }
+
+            v = function.apply(k, v);
+
+            try {
+                entry.setValue(v);
+            } catch (IllegalStateException ise) {
+                throw new ConcurrentModificationException(ise);
+            }
+        }
+    }
+
+    default V putIfAbsent(K key, V value) {
+        V v = get(key);
+        if (v == null) {
+            v = put(key, value);
+        }
+
+        return v;
+    }
+
+    default boolean remove(Object key, Object value) {
+        Object curValue = get(key);
+        if (!Objects.equals(curValue, value) ||
+                (curValue == null && !containsKey(key))) {
+            return false;
+        }
+
+        remove(key);
+        return true;
+    }
 
 }
