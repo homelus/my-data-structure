9d047e5fe7cd955920b2cd7b4afed0ab2adb838d
TMON\playjun <playjun@tmon.co.kr>
2019-11-21 11:20:45 +0900

    HashMap public operation & field

--- src/main/structure/HashMap.java
+++ src/main/structure/HashMap.java
@@ -1,6 +1,10 @@
 package structure;
 
 import java.io.Serializable;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.Collection;
+import java.util.Objects;
 import java.util.Set;
 
 /**
@@ -8,7 +12,182 @@ import java.util.Set;
  * @since 2019 11 20
  */
 public class HashMap<K,V> extends AbstractMap<K,V>
-    implements Map<K,V>, Cloneable, Serializable {
+        implements Map<K,V>, Cloneable, Serializable {
+
+    private static final long serialVersionUID = 362498820763181265L;
+
+    private final int DEFAULT_INITIAL_CAPACITY = 1 << 4;
+
+    static final int MAXIMUM_CAPACITY = 1 << 30;
+
+    static final float DEFAULT_LOAD_FACTOR = 0.75f;
+
+    static final int TREEIFY_THRESHHOLD = 8;
+
+    static final int UNTREEIFY_THRESHOLD = 6;
+
+    static final int MIN_TREEIFY_CAPACITY = 64;
+
+    static class Node<K, V> implements Map.Entry<K, V> {
+        final int hash;
+        final K key;
+        V value;
+        Node<K,V> next;
+
+        public Node(int hash, K key, V value, Node<K, V> next) {
+            this.hash = hash;
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+
+        @Override
+        public K getKey() {
+            return key;
+        }
+
+        @Override
+        public V getValue() {
+            return value;
+        }
+
+        @Override
+        public String toString() {
+            return key + "=" + value;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hashCode(key) ^ Objects.hashCode(value);
+        }
+
+        @Override
+        public V setValue(V newValue) {
+            V oldValue = value;
+            value = newValue;
+            return oldValue;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (o == this) {
+                return true;
+            }
+            if (o instanceof Map.Entry) {
+                Map.Entry<?,?> e = (Map.Entry<?,?>) o;
+                if (Objects.equals(key, e.getKey()) &&
+                    Objects.equals(value, e.getValue())) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
+    static final int hash(Object key) {
+        int h;
+        return (key == null) ? 0 : (h = key.hashCode()) ^ (h >>> 16);
+    }
+
+    static Class<?> comparableClassFor(Object x) {
+        if (x instanceof Comparable) {
+            Class<?> c;
+            Type[] ts, as;
+            Type t;
+            ParameterizedType p;
+
+            if ((c = x.getClass()) == String.class) {
+                return c;
+            }
+            if ((ts = c.getGenericInterfaces()) != null) {
+                for (int i = 0; i < ts.length; ++i) {
+                    if (((t = ts[i]) instanceof ParameterizedType) &&
+                            ((p = (ParameterizedType) t).getRawType() == Comparable.class) &&
+                            (as = p.getActualTypeArguments()) != null &&
+                            as.length == 1 && as[0] == c) {
+                        return c;
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    @SuppressWarnings({"rawtypes", "unchecked"})
+    static int compareComparables(Class<?> kc, Object k, Object x) {
+        return (x == null || x.getClass() != kc ? 0
+                : ((Comparable) k).compareTo(k));
+    }
+
+    static final int tableSizeFor(int cap) {
+        int n = cap - 1;
+        n |= n >>> 1;
+        n |= n >>> 2;
+        n |= n >>> 4;;
+        n |= n >>> 8;
+        n |= n >>> 16;
+        return (n < 0) ? 1 : (n >= MAXIMUM_CAPACITY) ? MAXIMUM_CAPACITY : n + 1;
+    }
+
+    /* Fields */
+
+    transient Node<K,V>[] table;
+
+    transient Set<Map.Entry<K,V>> entrySet;
+
+    transient int size;
+
+    transient int modCount;
+
+    int threshold;
+
+    final float loadFactor;
+
+    /* Public operations */
+
+    public HashMap(int initialCapacity, float loadFactor) {
+        if (initialCapacity < 0) {
+            throw new IllegalArgumentException("Illegal initial capacity: " + initialCapacity);
+        }
+
+        if (initialCapacity > MAXIMUM_CAPACITY) {
+            initialCapacity = MAXIMUM_CAPACITY;
+        }
+        if (loadFactor <= 0 || Float.isNaN(loadFactor)) {
+            throw new IllegalArgumentException("Illegal load factor: " + loadFactor);
+        }
+        this.loadFactor = loadFactor;
+        this.threshold = tableSizeFor(initialCapacity);
+    }
+
+    public HashMap(int initialCapacity) {
+        this(initialCapacity, DEFAULT_LOAD_FACTOR);
+    }
+
+    public HashMap() {
+        this.loadFactor = DEFAULT_LOAD_FACTOR;
+    }
+
+    @Override
+    public void putAll(Map<? extends K, ? extends V> m) {
+
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public Set<K> keySet() {
+        return null;
+    }
+
+    @Override
+    public Collection<V> values() {
+        return null;
+    }
+
     @Override
     public Set<Entry<K, V>> entrySet() {
         return null;
---
+++ src/test/structure/UtilTest.java
@@ -0,0 +1,19 @@
+package structure;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author playjun
+ * @since 2019 11 21
+ */
+public class UtilTest {
+
+    @Test
+    void XOR() {
+        int x = 10;
+        int y = 12;
+
+        System.out.println(x ^ y);
+    }
+
+}
