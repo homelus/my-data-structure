cdb18c6792ec3a2b6e631747f2e2587c96e44474
hyunjun <homelus@daum.net>
2019-12-13 17:57:54 +0900

    init Collector

---
+++ src/main/stream/Collector.java
@@ -0,0 +1,66 @@
+package stream;
+
+import java.util.Collections;
+import java.util.EnumSet;
+import java.util.Objects;
+import java.util.Set;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+public interface Collector<T, A, R> {
+
+    Supplier<A> supplier();
+
+    BiConsumer<A, T> accumulator();
+
+    BinaryOperator<A> combiner();
+
+    Function<A,R> finisher();
+
+    Set<Characteristics> characteristics();
+
+    public static <T, R> Collector<T, R, R> of(Supplier<R> supplier,
+                                               BiConsumer<R, T> accumulator,
+                                               BinaryOperator<R> combiner,
+                                               Characteristics... characteristics) {
+        Objects.requireNonNull(supplier);
+        Objects.requireNonNull(accumulator);
+        Objects.requireNonNull(combiner);
+        Objects.requireNonNull(characteristics);
+        Set<Characteristics> cs = (characteristics.length == 0)
+                ? Collectors.CH_ID
+                : Collections.unmodifiableSet(EnumSet.of(Characteristics.IDENTITY_FINISH,
+                characteristics));
+        return new Collectors.CollectorImpl<>(supplier, accumulator, combiner, cs);
+    }
+
+    public static <T, A, R> Collector<T, A, R> of(Supplier<A> supplier,
+                                                  BiConsumer<A, T> accumulator,
+                                                  BinaryOperator<A> combiner,
+                                                  Function<A, R> finish,
+                                                  Characteristics... characteristics) {
+        Objects.requireNonNull(supplier);
+        Objects.requireNonNull(accumulator);
+        Objects.requireNonNull(combiner);
+        Objects.requireNonNull(finish);
+        Objects.requireNonNull(characteristics);
+        Set<Characteristics> cs = Collectors.CH_NOID;
+        if (characteristics.length > 0) {
+            cs = EnumSet.noneOf(Characteristics.class);
+            Collections.addAll(cs, characteristics);
+            cs = Collections.unmodifiableSet(cs);
+        }
+        return new Collectors.CollectorImpl<>(supplier, accumulator, combiner, finish, cs);
+    }
+
+    enum Characteristics {
+
+        CONCURRENT,
+        UNORDERED,
+        IDENTITY_FINISH
+
+    }
+
+}
---
+++ src/main/stream/Collectors.java
@@ -0,0 +1,77 @@
+package stream;
+
+import java.util.Collections;
+import java.util.EnumSet;
+import java.util.Set;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+public final class Collectors {
+
+    static final Set<Collector.Characteristics> CH_CONCURRENT_ID
+            = Collections.unmodifiableSet(EnumSet.of(Collector.Characteristics.CONCURRENT,
+            Collector.Characteristics.UNORDERED,
+            Collector.Characteristics.IDENTITY_FINISH));
+
+    static final Set<Collector.Characteristics> CH_ID
+            = Collections.unmodifiableSet(EnumSet.of(Collector.Characteristics.IDENTITY_FINISH));
+
+    static final Set<Collector.Characteristics> CH_NOID = Collections.emptySet();
+
+    static class CollectorImpl<T,A,R> implements Collector<T,A,R> {
+        private final Supplier<A> supplier;
+        private final BiConsumer<A, T> accumulator;
+        private final BinaryOperator<A> combiner;
+        private final Function<A, R> finisher;
+        private final Set<Characteristics> characteristics;
+
+        CollectorImpl(Supplier<A> supplier, BiConsumer<A, T> accumulator, BinaryOperator<A> combiner, Function<A, R> finisher, Set<Characteristics> characteristics) {
+            this.supplier = supplier;
+            this.accumulator = accumulator;
+            this.combiner = combiner;
+            this.finisher = finisher;
+            this.characteristics = characteristics;
+        }
+
+        CollectorImpl(Supplier<A> supplier,
+                      BiConsumer<A, T> accumulator,
+                      BinaryOperator<A> combiner,
+                      Set<Characteristics> characteristics) {
+            this(supplier, accumulator, combiner, castingIdentity(), characteristics);
+        }
+
+        @SuppressWarnings("unchecked")
+        private static <A, R> Function<A,R> castingIdentity() {
+            return i -> (R) i;
+        }
+
+
+        @Override
+        public Supplier<A> supplier() {
+            return supplier;
+        }
+
+        @Override
+        public BiConsumer<A, T> accumulator() {
+            return accumulator;
+        }
+
+        @Override
+        public BinaryOperator<A> combiner() {
+            return combiner;
+        }
+
+        @Override
+        public Function<A, R> finisher() {
+            return finisher;
+        }
+
+        @Override
+        public Set<Characteristics> characteristics() {
+            return characteristics;
+        }
+    }
+
+}
